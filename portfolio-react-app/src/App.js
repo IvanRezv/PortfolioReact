5383373d75fd2b768f1af952c3d6c524b1a4f4bd
Ivan <ivanrezv@icloud.com>
2022-08-04 15:41:47 +0300

    init new component skills bar

--- portfolio-react-app/src/App.js
+++ portfolio-react-app/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import Navbar from "./components/navbar/navbar";
-import Home from "./components/home/home";
-import About from "./components/about/about";
+import Navbar from './components/navbar/navbar';
+import Home from './components/home/home';
+import About from './components/about/about';
+import Tools from './components/tools/tools';
 import Skills from './components/skills/skills';
 import Work from "./components/work/work";
-import Contact from "./components/contact/contact";
+import Contact from './components/contact/contact';
 import './app.css';
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
       <Home />
       <About />
       <Skills />
+      <Tools />
       <Work />
       <Contact />
     </div>
--- portfolio-react-app/src/components/skills/skills.jsx
+++ portfolio-react-app/src/components/skills/skills.jsx
@@ -2,70 +2,58 @@ import React,{ useCallback} from 'react';
 import Particles from "react-particles";
 import { loadFull } from "tsparticles";
 import particlesOptions from "../../particles.json";
+import './skills.css'
 
-
-import HTML from '../../assets/html.png';
-import CSS from '../../assets/css.png';
-import JavaScript from '../../assets/javascript.png';
-import ReactImg from '../../assets/react.png';
-import Node from '../../assets/node.png';
-import AWS from '../../assets/aws.png';
-import GitHub from '../../assets/github.png';
-import Mongo from '../../assets/mongo.png';
-
-
-const Skills = () => {
+const Progress = () => {
     const particlesInit = useCallback(main => {
         loadFull(main);
     }, [])
 
     return (
-        <div name='skills' className='w-full h-screen  text-gray-300'>
-            <Particles  options={particlesOptions} init={particlesInit} />
-            {/* Container */}
-            <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-container w-full h-full'>
-                <div >
-                    <p className='text-4xl font-bold inline border-b-4 border-pink-600 '>Skills</p>
-                    <p className='py-4'>These are tech I've workded with</p>
+    <div name='skills' className='w-full h-screen text-gray-300'>
+        <Particles  options={particlesOptions} init={particlesInit} />
+        {/* Container */}
+        <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-container w-full h-full'>
+            <div >
+                <p className='text-4xl font-bold inline border-b-4 border-pink-600 '>Skills</p>
+                <p className='py-4'>These are my experience in Persentage</p>
+            </div>
+            
+            <div className='skills-bar text-black'>
+                <div className='bar'>
+                    <div className='info'>
+                        <span>CI/CD</span>
+                    </div>
+                    <div className='progress-line cicd'><span></span></div>
                 </div>
-
-                <div className='w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8'>
-                    <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                        <img className='w-20 mx-auto' src={HTML} alt="HTML icon" />
-                        <p className='my-4'>HTML</p>
+                <div className='bar'>
+                    <div className='info'>
+                        <span>IaC</span>
                     </div>
-                    <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                        <img className='w-20 mx-auto' src={CSS} alt="HTML icon" />
-                        <p className='my-4'>CSS</p>
+                    <div className='progress-line iac'><span></span></div>
+                </div>
+                <div className='bar'>
+                    <div className='info'>
+                        <span>Unix</span>
                     </div>
-                    <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                        <img className='w-20 mx-auto' src={JavaScript} alt="HTML icon" />
-                        <p className='my-4'>JavaScript</p>
+                    <div className='progress-line unix'><span></span></div>
+                </div>
+                <div className='bar'>
+                    <div className='info'>
+                        <span>Python and Tooling</span>
                     </div>
-                    <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                        <img className='w-20 mx-auto' src={ReactImg} alt="HTML icon" />
-                        <p className='my-4'>React</p>
+                    <div className='progress-line python'><span></span></div>
+                </div>
+                <div className='bar'>
+                    <div className='info'>
+                        <span>Web Development</span>
                     </div>
-                    <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                        <img className='w-20 mx-auto' src={GitHub} alt="HTML icon" />
-                        <p className='my-4'>Github</p>
-                    </div>
-                    <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                        <img className='w-20 mx-auto' src={Node} alt="HTML icon" />
-                        <p className='my-4'>NodeJS</p>
-                    </div>
-                    <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                        <img className='w-20 mx-auto' src={Mongo} alt="HTML icon" />
-                        <p className='my-4'>MongoDb</p>
-                    </div>
-                    <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                        <img className='w-20 mx-auto' src={AWS} alt="HTML icon" />
-                        <p className='my-4'>AWS</p>
-                    </div>
+                    <div className='progress-line webdev'><span></span></div>
                 </div>
             </div>
         </div>
-    )
-}
+    </div>
+  );
+};
 
-export default Skills
+export default Progress
